d41d52935ddebf11b089ed4d3356aa694162e93d
Evas1oN <51102449+Evas1oN@users.noreply.github.com>
2024-12-02 13:48:15 +0300

    Revert "Добавлен LDAPConnector, обновлен сертификат"

--- api/handlers/user_handler.go
+++ api/handlers/user_handler.go
@@ -4,35 +4,10 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-ldap/ldap/v3"
-	ldapconnector "github.com/klambri/ldap-api-bridge/internal/ldap-connector"
 )
 
 func GetUsers(c *gin.Context) {
-	// todo: Need refactoring
-	connector, err := ldapconnector.CreateConnector("ldaps://klambri.lan", "Administrator@klambri.lan", "P@ssw0rd")
-	if err != nil {
-		c.JSON(http.StatusServiceUnavailable, "error")
-	}
-	defer connector.Close()
-
-	searchRequest := ldap.NewSearchRequest(
-		"cn=Users,dc=klambri,dc=lan", ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false, "(|(objectClass=person)(objectClass=user))", []string{"cn", "dn"}, nil,
-	)
-
-	result, err := connector.Search(searchRequest)
-	if err != nil {
-		c.JSON(http.StatusNotFound, "Not Found")
-	}
-
-	resp := []string{}
-	for _, entry := range result.Entries {
-		resp = append(resp, entry.DN)
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"users": resp,
-	})
+	c.JSON(http.StatusOK, "get_users")
 }
 
 func CreateUser(c *gin.Context) {
